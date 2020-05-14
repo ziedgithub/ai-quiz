2873ed643b94810976f16286e8fa1d50f69bb166
Zied <ziedbenamar6@gmail.com>
2020-05-14 02:27:43 +0200

    Updates

---
+++ precache-manifest.69fc0e105f1301fb7c4e41f08b685f56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d468c3f7af8b4c5d5f0207082b57f77d",
+    "url": "/ai-quiz/index.html"
+  },
+  {
+    "revision": "11f25408429b3b816ab9",
+    "url": "/ai-quiz/static/css/main.25d87324.chunk.css"
+  },
+  {
+    "revision": "629621aeaa940a13e5e0",
+    "url": "/ai-quiz/static/js/2.6f2ca652.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ai-quiz/static/js/2.6f2ca652.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11f25408429b3b816ab9",
+    "url": "/ai-quiz/static/js/main.b30ba17e.chunk.js"
+  },
+  {
+    "revision": "58e50cabaf0887dbf94e",
+    "url": "/ai-quiz/static/js/runtime-main.6ce6e251.js"
+  }
+]);
